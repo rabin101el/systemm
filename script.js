65f30fcce393efadb81263aa24f27acf0f0b9db6
systemm <rabinkandel472@gmail.com>
2023-09-17 08:09:14 +0545

    added

--- script.js
+++ script.js
@@ -6,7 +6,7 @@ sendButton.addEventListener("click", sendMessage);
 
 function sendMessage() {
   const userMessage = userMessageInput.value.trim();
-  if (userMessage == "LeTaG1J") {
+  if (userMessage == "YKO9zF") {
     addUserMessage(userMessage);
     // Replace the bot's response with an actual response from your chatbot service or logic
     addBotMessage("prophet_muhammad_is_pedophile@mail.ru");
